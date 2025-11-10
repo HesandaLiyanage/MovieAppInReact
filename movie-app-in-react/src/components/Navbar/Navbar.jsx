d473d84f3222cbb66411580ca0f8ed15cf9af6f3
Hesanda Liyanage <nhesanda@gmail.com>
2025-11-10 20:22:26 +0530

    10 or somthn

--- movie-app-in-react/src/components/Navbar/Navbar.jsx
+++ movie-app-in-react/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import './Navbar.css'
+import logo from '../../assets/logo.png'
 
 
 const Navbar= () => {
     render (
         <div className='navbar'>
-
+            <div className="navbar-left">
+                nkobo
+                <img src={logo} alt="" />
+            </div>
+            <div className="navbar-right"></div>
         </div>
     )
 }
