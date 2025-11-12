46e0950a884cfef8845e2fc7a62c06951b04ed56
Hesanda Liyanage <nhesanda@gmail.com>
2025-11-12 13:51:04 +0530

    16

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import './Footer.jsx'
 import youtube_icon from '../../assets/youtube_icon.png'
+import twiiter_icon from '../../assets/twitter_icon.png'
+import instagram_icon from '../../assets/instagram_icon.png'
+import facebook_icon from '../../assets/facebook_icon.png'
+
 
 
 const Footer = ()=> {
     render(
         <div className = 'footer'>
             <div className="footer-icons">
-                <img src={} alt="" />
+                <img src={youtube_icon} alt="" />
             </div>
         </div>
     )
