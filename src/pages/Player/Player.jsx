fa3cd7368af9962b901d611a1ffb043ac8bf952a
Hesanda Liyanage <nhesanda@gmail.com>
2025-11-14 09:00:08 +0530

    6

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -4,7 +4,7 @@ import back_arrow_icon from '../../assets/back_arrow_icon.png'
 
 
 const Player = () => {
-    render (
+    return (
         <div className='player'>
             <img src={back_arrow_icon} alt="" />
             <iframe width='90%' height='90%'
