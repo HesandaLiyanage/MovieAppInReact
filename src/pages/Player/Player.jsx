b87ae77835caecdb49862871faa806eacc23fa10
Hesanda Liyanage <nhesanda@gmail.com>
2025-11-15 12:31:29 +0530

    11

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -6,7 +6,7 @@ import {useParams} from 'react-router-dom'
 
 const Player = () => {
 
-    const {id} = useParams
+    const {id} = useParams()
 
     const [apiData, setApiData] = useState({
         name:"",
