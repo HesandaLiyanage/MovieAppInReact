3416da3b217197c95ee2752f7ce038e48bf50bbc
Hesanda Liyanage <nhesanda@gmail.com>
2025-11-15 11:59:29 +0530

    1

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import './Player.css'
 import back_arrow_icon from '../../assets/back_arrow_icon.png'
 
 
 const Player = () => {
+
+    const [apiData, setApiData] = useState({
+        name:"",
+        key:"",
+        
+
+    })
+
+    const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNjA1MGMzOTdjZjViMDZiOWIwOGIxOTYyNWQzM2Y2YSIsIm5iZiI6MTc2Mjk0OTU1Ni40NTgsInN1YiI6IjY5MTQ3OWI0YTc3MGZmMzhjNWMwNmUwZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.X2d3M0-dvs5_EaBDBArlJDZeEQxqlhaF-VHHcdII1bI'
+  }
+};
+
+    useEffect(() => {
+        fetch('https://api.themoviedb.org/3/movie/1062722/videos?language=en-US', options)
+        .then(res => res.json())
+        .then(res => console.log(res))
+  .catch(err => console.error(err));
+    }, [])
+
     return (
         <div className='player'>
             <img src={back_arrow_icon} alt="" />
